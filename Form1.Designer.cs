0b25f43e0306776ca8549244789d098176d4ab52
Jarrett <41562597+weathrman@users.noreply.github.com>
2024-09-23 12:07:46 -0400

    changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            InactiveNotificationIcon = new PictureBox();
+            ActiveNotificationIcon = new PictureBox();
+            ShowAdminPanelButton = new Button();
+            AdminPanel = new Panel();
+            NotificationPanel = new Panel();
+            CloseNotificationPanelButton = new Button();
+            AdminPanelInactiveNotificationIcon = new Button();
+            AdminPanelShowActiveNotification = new Button();
+            ((System.ComponentModel.ISupportInitialize)InactiveNotificationIcon).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ActiveNotificationIcon).BeginInit();
+            AdminPanel.SuspendLayout();
+            NotificationPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // InactiveNotificationIcon
+            // 
+            InactiveNotificationIcon.BackgroundImage = (Image)resources.GetObject("InactiveNotificationIcon.BackgroundImage");
+            InactiveNotificationIcon.BackgroundImageLayout = ImageLayout.Stretch;
+            InactiveNotificationIcon.Cursor = Cursors.Hand;
+            InactiveNotificationIcon.Location = new Point(1296, 12);
+            InactiveNotificationIcon.Name = "InactiveNotificationIcon";
+            InactiveNotificationIcon.Size = new Size(125, 125);
+            InactiveNotificationIcon.TabIndex = 0;
+            InactiveNotificationIcon.TabStop = false;
+            InactiveNotificationIcon.Visible = false;
+            // 
+            // ActiveNotificationIcon
+            // 
+            ActiveNotificationIcon.BackgroundImage = (Image)resources.GetObject("ActiveNotificationIcon.BackgroundImage");
+            ActiveNotificationIcon.BackgroundImageLayout = ImageLayout.Stretch;
+            ActiveNotificationIcon.Cursor = Cursors.Hand;
+            ActiveNotificationIcon.Location = new Point(1296, 12);
+            ActiveNotificationIcon.Name = "ActiveNotificationIcon";
+            ActiveNotificationIcon.Size = new Size(125, 125);
+            ActiveNotificationIcon.TabIndex = 1;
+            ActiveNotificationIcon.TabStop = false;
+            ActiveNotificationIcon.Visible = false;
+            ActiveNotificationIcon.Click += ActiveNotificationIcon_Click;
+            // 
+            // ShowAdminPanelButton
+            // 
+            ShowAdminPanelButton.BackColor = SystemColors.Control;
+            ShowAdminPanelButton.ForeColor = SystemColors.ControlText;
+            ShowAdminPanelButton.Location = new Point(1066, 754);
+            ShowAdminPanelButton.Name = "ShowAdminPanelButton";
+            ShowAdminPanelButton.Size = new Size(343, 40);
+            ShowAdminPanelButton.TabIndex = 2;
+            ShowAdminPanelButton.Text = "Show Admin Panel";
+            ShowAdminPanelButton.UseVisualStyleBackColor = false;
+            ShowAdminPanelButton.Click += ShowAdminPanelButton_Click;
+            // 
+            // AdminPanel
+            // 
+            AdminPanel.Controls.Add(NotificationPanel);
+            AdminPanel.Controls.Add(AdminPanelInactiveNotificationIcon);
+            AdminPanel.Controls.Add(AdminPanelShowActiveNotification);
+            AdminPanel.Location = new Point(-5, -3);
+            AdminPanel.Name = "AdminPanel";
+            AdminPanel.Size = new Size(1065, 797);
+            AdminPanel.TabIndex = 0;
+            AdminPanel.Visible = false;
+            // 
+            // NotificationPanel
+            // 
+            NotificationPanel.Controls.Add(CloseNotificationPanelButton);
+            NotificationPanel.Location = new Point(3, 3);
+            NotificationPanel.Name = "NotificationPanel";
+            NotificationPanel.Size = new Size(1423, 806);
+            NotificationPanel.TabIndex = 2;
+            NotificationPanel.Visible = false;
+            // 
+            // CloseNotificationPanelButton
+            // 
+            CloseNotificationPanelButton.BackColor = SystemColors.Control;
+            CloseNotificationPanelButton.ForeColor = SystemColors.ControlText;
+            CloseNotificationPanelButton.Location = new Point(14, 751);
+            CloseNotificationPanelButton.Name = "CloseNotificationPanelButton";
+            CloseNotificationPanelButton.Size = new Size(131, 40);
+            CloseNotificationPanelButton.TabIndex = 3;
+            CloseNotificationPanelButton.Text = "Close";
+            CloseNotificationPanelButton.UseVisualStyleBackColor = false;
+            CloseNotificationPanelButton.Visible = false;
+            CloseNotificationPanelButton.Click += CloseNotificationPanelButton_Click;
+            // 
+            // AdminPanelInactiveNotificationIcon
+            // 
+            AdminPanelInactiveNotificationIcon.BackColor = SystemColors.Control;
+            AdminPanelInactiveNotificationIcon.ForeColor = SystemColors.ControlText;
+            AdminPanelInactiveNotificationIcon.Location = new Point(17, 83);
+            AdminPanelInactiveNotificationIcon.Name = "AdminPanelInactiveNotificationIcon";
+            AdminPanelInactiveNotificationIcon.Size = new Size(443, 40);
+            AdminPanelInactiveNotificationIcon.TabIndex = 1;
+            AdminPanelInactiveNotificationIcon.Text = "Show Inactive Notification";
+            AdminPanelInactiveNotificationIcon.UseVisualStyleBackColor = false;
+            AdminPanelInactiveNotificationIcon.Click += AdminPanelInactiveNotificationIcon_Click;
+            // 
+            // AdminPanelShowActiveNotification
+            // 
+            AdminPanelShowActiveNotification.BackColor = SystemColors.Control;
+            AdminPanelShowActiveNotification.ForeColor = SystemColors.ControlText;
+            AdminPanelShowActiveNotification.Location = new Point(17, 26);
+            AdminPanelShowActiveNotification.Name = "AdminPanelShowActiveNotification";
+            AdminPanelShowActiveNotification.Size = new Size(443, 40);
+            AdminPanelShowActiveNotification.TabIndex = 0;
+            AdminPanelShowActiveNotification.Text = "Show Active Notification";
+            AdminPanelShowActiveNotification.UseVisualStyleBackColor = false;
+            AdminPanelShowActiveNotification.Click += AdminPanelShowActiveNotification_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1421, 806);
+            Controls.Add(AdminPanel);
+            Controls.Add(ShowAdminPanelButton);
+            Controls.Add(ActiveNotificationIcon);
+            Controls.Add(InactiveNotificationIcon);
+            ForeColor = SystemColors.Control;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)InactiveNotificationIcon).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ActiveNotificationIcon).EndInit();
+            AdminPanel.ResumeLayout(false);
+            NotificationPanel.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox InactiveNotificationIcon;
+        private PictureBox ActiveNotificationIcon;
+        private Button ShowAdminPanelButton;
+        private Panel AdminPanel;
+        private Button AdminPanelInactiveNotificationIcon;
+        private Button AdminPanelShowActiveNotification;
+        private Panel NotificationPanel;
+        private Button CloseNotificationPanelButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,59 @@ namespace CSC340PharmacyGroupProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowAdminPanelButton_Click(object sender, EventArgs e)
+        {
+            if (AdminPanel.Visible)
+            {
+                AdminPanel.Visible = false;
+            }
+            else
+            {
+                AdminPanel.Visible = true;
+                AdminPanelShowActiveNotification.Visible = true;
+                AdminPanelInactiveNotificationIcon.Visible = true;
+            }
+        }
+
+        private void AdminPanelShowActiveNotification_Click(object sender, EventArgs e)
+        {
+            InactiveNotificationIcon.Visible = false;
+            ActiveNotificationIcon.Visible = true;
+        }
+
+        private void AdminPanelInactiveNotificationIcon_Click(object sender, EventArgs e)
+        {
+            InactiveNotificationIcon.Visible = true;
+            ActiveNotificationIcon.Visible = false;
+        }
+
+        private void ActiveNotificationIcon_Click(object sender, EventArgs e)
+        {
+            ShowNotificationCenter();
+
+        }
+
+        private void InActiveNotificationIcon_Click(object sender, EventArgs e)
+        {
+            ShowNotificationCenter();
+        }
+
+        private void ShowNotificationCenter()
+        {
+            NotificationPanel.Visible = true;
+            CloseNotificationPanelButton.Visible = true;
+        }
+
+        private void CloseNotificationPanelButton_Click(object sender, EventArgs e)
+        {
+            NotificationPanel.Visible = false;
+            CloseNotificationPanelButton.Visible = false;
+        }
     }
 }
